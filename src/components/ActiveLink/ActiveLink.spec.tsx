02c30430908b5d9713666aeb11f1d23cc325af19
João Matos <joaomarcosm.silva@outlook.com>
2023-08-12 09:52:02 -0300

    config jest and create test for Active Link

---
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -0,0 +1,34 @@
+import { render } from "@testing-library/react";
+import { ActiveLink } from ".";
+
+jest.mock("next/router", () => {
+  return {
+    useRouter() {
+      return {
+        asPath: "/",
+      };
+    },
+  };
+});
+
+describe("Active Link Component", () => {
+  it("renders correctly", () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+
+    expect(getByText("Home")).toBeInTheDocument();
+  });
+
+  it("adds ative class if the link as currently", () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+
+    expect(getByText("Home")).toHaveClass("active");
+  });
+});

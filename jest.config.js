e802c41e8cf91d3ed30684092663b7e849d089be
João Matos <joaomarcosm.silva@outlook.com>
2023-08-14 21:07:18 -0300

    create test for home page

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testPathIgnorePatterns: ["/node_modules/", "/.next/"],
   setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)": "<rootDir>/node_modules/babel-jest",
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
   },
 
   moduleNameMapper: {
